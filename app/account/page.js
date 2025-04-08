72a99d363052472ee5f3c2ce3d0f86260e577c2e
Muziqa79 <muziqa@gmail.com>
2025-04-09 00:32:09 +0100

    Add files via upload

---
+++ app/account/page.js
@@ -0,0 +1,9 @@
+"use client";
+export default function AccountPage() {
+  return (
+    <main style={ padding: '2rem' }>
+      <h1>Account Page</h1>
+      <p>This is the account page placeholder.</p>
+    </main>
+  );
+}
---
+++ app/genre/page.js
@@ -0,0 +1,9 @@
+"use client";
+export default function GenrePage() {
+  return (
+    <main style={ padding: '2rem' }>
+      <h1>Genre Page</h1>
+      <p>This is the genre page placeholder.</p>
+    </main>
+  );
+}
