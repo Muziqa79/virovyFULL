7eef2028e33349f1324bd9533d3c854a4c2ba44e
Muziqa79 <muziqa@gmail.com>
2025-04-09 00:43:16 +0100

    Delete pages directory

--- pages/api/engagement.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Engagement score system incoming." });
-}
--- pages/api/tiktok.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "TikTok trending data will appear here." });
-}
