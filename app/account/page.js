3a0e0c0ced34b023327128128558c8bef6e19d9b
Muziqa79 <muziqa@gmail.com>
2025-04-09 00:35:44 +0100

    Update page.js

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,7 @@
 "use client";
 export default function AccountPage() {
   return (
-    <main style={ padding: '2rem' }>
+    <main style={{ padding: '2rem' }}>
       <h1>Account Page</h1>
       <p>This is the account page placeholder.</p>
     </main>
